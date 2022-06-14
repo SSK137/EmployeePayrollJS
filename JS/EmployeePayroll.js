36df5f07a6bcf3a29e0603c2b1d35389aabf0f76
SSK137 <saurabhkodam@gmail.com>
2022-06-15 00:06:45 +0530

    Added Event Listener Method to check salary range and validate name

--- JS/EmployeePayroll.js
+++ JS/EmployeePayroll.js
@@ -1,8 +1,22 @@
-const salary = document.querySelector('#salary');
-        const output = document.querySelector('.salary-output');
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        try {
+            (new EmployeePayroll()).name = name.value;
+             textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
         output.textContent = salary.value;
-        salary.addEventListener('input', function () {
-            output.textContent = salary.value;
+    });
+
 });
 class EmployeePayroll {
     get id() { return this._id; }
