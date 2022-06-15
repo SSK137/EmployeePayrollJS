17d48277885e957a14fffb32f7db1b9976a409de
SSK137 <saurabhkodam@gmail.com>
2022-06-15 17:10:34 +0530

    Added Method to validate the Name and Date

--- JS/EmployeePayroll.js
+++ JS/EmployeePayroll.js
@@ -1,7 +1,27 @@
+
 window.addEventListener('DOMContentLoaded', (event) => {
+    validateDate();
+    validatename();
+    salaryrange();
+    
+});
+function salaryrange(){
+    const salary = document.querySelector('#salary');
+        const output = document.querySelector('.salary-output');
+        output.textContent = salary.value;
+        salary.addEventListener('input', function () {
+            output.textContent = salary.value;
+});
+
+}
+function validatename(){
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
     name.addEventListener('input', function () {
+        if(name.value.length==0){
+            textError.textContent = "";
+            return;
+        }
         try {
             (new EmployeePayroll()).name = name.value;
              textError.textContent = "";
@@ -9,71 +29,38 @@ window.addEventListener('DOMContentLoaded', (event) => {
             textError.textContent = e;
         }
     });
+} 
 
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
-    output.textContent = salary.value;
-    salary.addEventListener('input', function () {
-        output.textContent = salary.value;
-    });
+function validateDate(){
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
 
-});
-class EmployeePayroll {
-    get id() { return this._id; }
+    day.addEventListener('input',checkdate)
+    month.addEventListener('input',checkdate)
+    year.addEventListener('input',checkdate)
 
-    set id(id) {
-        this.id = id;
-    }
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        let nameRgx = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if (nameRgx.test(name))
-            this._name = name;
-        else throw 'Name is Incorrect!';
-    }
-    get profilePic() {
-        return this._profilePic;
-    }
-    set profilePic(profilePic) {
-        this._profilePic = profilePic;
-    }
-
-    get gender() {
-        return this._gender;
-    }
-    set gender(gender) {
-        this._gender = gender;
-    }
-    get department() {
-        return this._department;
-    }
-    set department(department) {
-        this._department = department;
-    }
-    get salary() {
-        return this._salary;
-    }
-    set salary(salary) {
-        this._salary = salary;
-    }
-    get note() {
-        return this._note;
-    }
-    set note(note) {
-        this._note = note;
-    }
-    get startDate() {
-        return this._startDate;
-    }
-    set startDate(startDate) {
-        this._startDate = startDate;
-    }
-
-    toString() {
-        return "id=" + this.id + ", name='" + this.name + ", gender=" + this.gender +
-            ", profilePic=" + this.profilePic + ", department=" + this.department +
-            ",salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
-    }
 }
+
+function checkdate(){
+    const dateError = document.querySelector('.date-error');
+ try{
+    let date=day.value+""+month.value+""+year.value;
+    checkStartDate(date);
+    dateError.textContent="";
+ }
+ catch(e)
+ {
+    dateError.textContent=e;
+ }
+}
+checkStartDate=(date)=>{
+    let curruntDate = new Date();
+    let startDate=new Date(date)
+    if(startDate> curruntDate)
+    throw "Start Date iS future Date";
+    const diff = Math.abs(curruntDate.getTime() - startDate.getTime());
+    if(diff/(1000*60*60*24)>30)
+    throw "Start Date is beyond 30 Days";
+}
+
